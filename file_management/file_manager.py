863c41060a7ff9aad028c1541587ba29c5f2fc7f
Yasin Ughur (Uğur) <44711227+ysnghr@users.noreply.github.com>
2024-05-06 15:33:30 +0300

    Modify FileManager for supporting true format of file types

--- file_management/file_manager.py
+++ file_management/file_manager.py
@@ -1,3 +1,18 @@
+import magic
+
+
 class FileManager:
-    def identify_file_type(self, file):
-        pass
+    def __init__(self):
+        self.magic = magic.Magic(mime=True)
+        self.mime_to_extension = {
+            'application/pdf': 'pdf',
+            'application/vnd.openxmlformats-officedocument.wordprocessingml.document': 'docx',
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': 'xlsx',
+            'application/vnd.openxmlformats-officedocument.presentationml.presentation': 'pptx',
+            # TODO: Add more MIME types and corresponding file extensions as needed
+        }
+
+    def identify_file_type(self, file_path):
+        mime_type = self.magic.from_file(file_path)
+        file_extension = self.mime_to_extension.get(mime_type, '')  # Get corresponding extension
+        return file_extension if file_extension else mime_type
