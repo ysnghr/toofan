c1c06f2851efbfb114d8ef3760367465f3d1ef7b
Yasin Ughur (Uğur) <44711227+ysnghr@users.noreply.github.com>
2024-05-06 17:12:13 +0300

    Add TooFanAnalyzer

---
+++ analyzers/toofan_analyzer.py
@@ -0,0 +1,32 @@
+# pylint:disable=too-few-public-methods
+"""
+This module contains the TooFanAnalyzer class for analyzing
+files using different analyzers.
+"""
+from file_management.file_manager import FileManager
+from factories.analyzer_factory import AnalyzerFactory
+
+
+class TooFanAnalyzer:
+    """This class performs analysis on files using different analyzers."""
+    def __init__(self, file_manager=FileManager(), analyzer_factory=AnalyzerFactory()):
+        self.file_manager = file_manager
+        self.analyzer_factory = analyzer_factory
+
+    def analyze_files(self, files):
+        """
+        Analyze the given files.
+
+        Args:
+            files (list): List of file paths to analyze.
+
+        Returns:
+            list: List of analysis results for each file.
+        """
+        results = []
+        for file in files:
+            file_type = self.file_manager.identify_file_type(file)
+            analyzer = self.analyzer_factory.get_analyzer(file_type)
+            result = analyzer.analyze(file)
+            results.append(result)
+        return results
